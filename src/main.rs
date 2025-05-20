aa50658ce225e121edef389e91c1b7acef2d9f3c
carl <ccheartszzz@gmail.com>
2025-05-21 01:01:15 +0800

    feat: add terminal select

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,72 @@
 mod platform;
 use crate::platform::StartupType;
+use dialoguer::{Select, theme::ColorfulTheme};
+use std::error::Error;
 
-fn main() {
+#[derive(Debug)]
+struct OptionItem {
+    label: String,
+    value: String,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
     println!("BootWatch - startup item scanner\n");
 
     let items = platform::get_all_startup_items();
 
     println!("📦 共发现 {} 个开机启动项：\n", items.len());
+    let mut select_ret: Vec<OptionItem> = vec![];
     for item in items {
         match item.item_type {
             StartupType::Plist => {
-                println!(
-                    "📝 {} ({})",
-                    item.label,
-                    item.path.as_deref().unwrap_or("未知路径")
-                );
+                select_ret.push(OptionItem {
+                    label: format!(
+                        "📝 {} ({})",
+                        item.label,
+                        item.path.as_deref().unwrap_or("未知路径")
+                    ),
+                    value: item.path.as_deref().unwrap_or("").to_string(),
+                });
             }
             StartupType::LoginItem => {
-                println!("🚀 {} (Login Item)", item.label);
+                select_ret.push(OptionItem {
+                    label: format!("🚀 {} (Login Item)", item.label,),
+                    value: item.label,
+                });
             }
             StartupType::Registry => {
-                println!(
-                    "🔑 {} (Registry, {})",
-                    item.label,
-                    item.path.as_deref().unwrap_or("未知路径")
-                );
+                select_ret.push(OptionItem {
+                    label: format!(
+                        "🔑 {} (Registry, {})",
+                        item.label,
+                        item.path.as_deref().unwrap_or("未知路径")
+                    ),
+                    value: item.path.as_deref().unwrap_or("").to_string(),
+                });
             }
             StartupType::StartupFolder => {
-                println!(
-                    "📂 {} (Startup Folder, {})",
-                    item.label,
-                    item.path.as_deref().unwrap_or("未知路径")
-                );
+                select_ret.push(OptionItem {
+                    label: format!(
+                        "📂 {} (Startup Folder, {})",
+                        item.label,
+                        item.path.as_deref().unwrap_or("未知路径")
+                    ),
+                    value: item.path.as_deref().unwrap_or("").to_string(),
+                });
             }
         }
     }
+
+    let labels: Vec<&str> = select_ret.iter().map(|item| &item.label[..]).collect();
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .with_prompt("请选择一个选项")
+        .items(&labels)
+        .default(0)
+        .interact()?;
+
+    let selected = &select_ret[selection];
+    println!("你选择了: {} (值: {})", selected.label, selected.value);
+
+    Ok(())
 }
